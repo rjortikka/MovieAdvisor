cca74c8e5ab56a2d0d409c29dd9b871a74d14ada
rjortikka <60109352+rjortikka@users.noreply.github.com>
2022-04-26 17:40:04 +0300

    Update MainActivity.java
    test

--- app/src/main/java/com/example/movieadvisor/MainActivity.java
+++ app/src/main/java/com/example/movieadvisor/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }/////
 
     public void loadActivity(View v) {
         Intent intent = new Intent(MainActivity.this, Password.class);
